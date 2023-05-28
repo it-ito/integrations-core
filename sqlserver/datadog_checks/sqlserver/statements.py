e40863913118edd3ccf5ade50c2cd301650e2631
it-ito <itito.tec@gmail.com>
2023-05-28 18:29:32 +0900

    fix2

--- sqlserver/datadog_checks/sqlserver/statements.py
+++ sqlserver/datadog_checks/sqlserver/statements.py
@@ -55,43 +55,57 @@ SQL_SERVER_QUERY_METRICS_COLUMNS = [
 STATEMENT_METRICS_QUERY = """\
 with qstats as (
     select query_hash, query_plan_hash, last_execution_time, last_elapsed_time,
-            CONCAT(
+            plan_handle AS plan_handle_orig,
+            statement_start_offset AS statement_start_offset_orig,
+            statement_end_offset AS statement_end_offset_orig,
+/*            CONCAT(
                 CONVERT(nvarchar, plan_handle), ',',
                 CONVERT(nvarchar, statement_start_offset), ',',
-                CONVERT(nvarchar, statement_end_offset)) as plan_handle_and_offsets,
+                CONVERT(nvarchar, statement_end_offset)) as plan_handle_and_offsets,*/
+            'aaaa' as plan_handle_and_offsets,
            (select value from sys.dm_exec_plan_attributes(plan_handle) where attribute = 'dbid') as dbid,
            {query_metrics_columns}
     from sys.dm_exec_query_stats
 ),
 qstats_aggr as (
     select query_hash, query_plan_hash, CAST(S.dbid as int) as dbid,
-       D.name as database_name, max(plan_handle_and_offsets) as plan_handle_and_offsets,
+       D.name as database_name,
+       max(plan_handle_and_offsets) as plan_handle_and_offsets,
        max(last_execution_time) as last_execution_time,
        max(last_elapsed_time) as last_elapsed_time,
+       max(plan_handle_orig) as plan_handle_orig,
+       max(statement_start_offset_orig) as statement_start_offset_orig,
+       max(statement_end_offset_orig) as statement_end_offset_orig,
     {query_metrics_column_sums}
     from qstats S
     left join sys.databases D on S.dbid = D.database_id
     group by query_hash, query_plan_hash, S.dbid, D.name
 ),
 qstats_aggr_split as (select TOP {limit}
-    convert(varbinary(64),substring(plan_handle_and_offsets, 1, first_comma_index - 1), 0) as plan_handle,
-    convert(int,substring(plan_handle_and_offsets, first_comma_index + 1, second_comma_index - first_comma_index - 1)) as statement_start_offset,
-    convert(int,substring(plan_handle_and_offsets, second_comma_index + 1, LEN(plan_handle_and_offsets) - second_comma_index)) as statement_end_offset,
+--    convert(varbinary(64),substring(plan_handle_and_offsets, 1, first_comma_index - 1), 0) as plan_handle,
+--    convert(int,substring(plan_handle_and_offsets, first_comma_index + 1, second_comma_index - first_comma_index - 1)) as statement_start_offset,
+--    convert(int,substring(plan_handle_and_offsets, second_comma_index + 1, LEN(plan_handle_and_offsets) - second_comma_index)) as statement_end_offset,
     * from qstats_aggr
-	cross apply (select CHARINDEX(',', plan_handle_and_offsets , 1) as first_comma_index) as comma_position1
-	cross apply (select CHARINDEX(',', plan_handle_and_offsets , first_comma_index+1)  as second_comma_index) as comma_position2
+--	outer apply (select CHARINDEX(',', plan_handle_and_offsets , 1) as first_comma_index) as comma_position1
+--	outer apply (select CHARINDEX(',', plan_handle_and_offsets , first_comma_index + 1)  as second_comma_index) as comma_position2
     where DATEADD(ms, last_elapsed_time / 1000, last_execution_time) > dateadd(second, -?, getdate())
 )
 select
-    SUBSTRING(text, (statement_start_offset / 2) + 1,
+/*    SUBSTRING(text, (statement_start_offset / 2) + 1,
     ((CASE statement_end_offset
         WHEN -1 THEN DATALENGTH(text)
         ELSE statement_end_offset END
-            - statement_start_offset) / 2) + 1) AS statement_text,
+            - statement_start_offset) / 2) + 1) AS statement_text,*/
+    qt.text as statement_text,
+    statement_start_offset_orig as statement_start_offset,
+    statement_end_offset_orig as statement_end_offset,
+    0 as first_comma_index,
+    0 as second_comma_index,
+    plan_handle_orig as plan_handle,
     qt.text,
     encrypted as is_encrypted,
     * from qstats_aggr_split
-    cross apply sys.dm_exec_sql_text(plan_handle) qt
+    cross apply sys.dm_exec_sql_text(plan_handle_orig) qt
 """
 
 # This query is an optimized version of the statement metrics query
@@ -100,21 +114,19 @@ STATEMENT_METRICS_QUERY_NO_AGGREGATES = """\
 with qstats_aggr as (
     select TOP {limit} query_hash, query_plan_hash,
         max(CONCAT(
-            CONVERT(nvarchar, plan_handle), ',',
-            CONVERT(nvarchar, statement_start_offset), ',',
-            CONVERT(nvarchar, statement_end_offset))) as plan_handle_and_offsets,
+            CONVERT(binary(64), plan_handle),
+            CONVERT(binary(4), statement_start_offset),
+            CONVERT(binary(4), statement_end_offset))) as plan_handle_and_offsets,
         {query_metrics_column_sums}
         from sys.dm_exec_query_stats S
         where last_execution_time > dateadd(second, -?, getdate())
         group by query_hash, query_plan_hash
 ),
 qstats_aggr_split as (select
-    convert(varbinary(64),substring(plan_handle_and_offsets, 1, first_comma_index - 1), 0) as plan_handle,
-    convert(int,substring(plan_handle_and_offsets, first_comma_index + 1, second_comma_index - first_comma_index - 1)) as statement_start_offset,
-    convert(int,substring(plan_handle_and_offsets, second_comma_index + 1, LEN(plan_handle_and_offsets) - second_comma_index)) as statement_end_offset,
+    convert(varbinary(64), substring(plan_handle_and_offsets, 1, 64)) as plan_handle,
+    convert(int, convert(varbinary(4), substring(plan_handle_and_offsets, 64+1, 4))) as statement_start_offset,
+    convert(int, convert(varbinary(4), substring(plan_handle_and_offsets, 64+6, 4))) as statement_end_offset,
     * from qstats_aggr
-	cross apply (select CHARINDEX(',', plan_handle_and_offsets , 1) as first_comma_index) as comma_position1
-	cross apply (select CHARINDEX(',', plan_handle_and_offsets , first_comma_index+1)  as second_comma_index) as comma_position2
 )
 select
     SUBSTRING(text, (statement_start_offset / 2) + 1,
